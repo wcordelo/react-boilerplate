d0e9eb58d8afaab67aa99e083891f9f1712de1cc
William Lopez-Cordero <williamlopezc@gmail.com>
2022-05-18 11:58:05 -0400

    Added breaches example

--- src/sample/index.js
+++ src/sample/index.js
@@ -1,3 +1,3 @@
 export { default as users } from "./users";
-export { default as breaches } from "./breaches";
-export { default as response } from "./response";
+export { default as breaches } from "./breaches.json";
+export { default as response } from "./response.json";
